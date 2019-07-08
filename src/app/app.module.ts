eee54cc9c1dbf491584d2d12a2b5652ac7f2883e
Patrick <revmydev@gmail.com>
2019-07-08 12:49:11 -0700

    missed declarations from last commit; game buttons component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameButtonsComponent } from './game-buttons/game-buttons.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameButtonsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
